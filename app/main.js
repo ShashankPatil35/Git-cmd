560c52537cdec29cd47f0f74c57974b168b1e8c0
ShashankPatil35 <shashankmpatil3535@gmail.com>
2024-10-10 20:32:25 +0530

    readGitBlob

--- app/main.js
+++ app/main.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const path = require("path");
+const zlib = require("zlib")
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+// console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const command = process.argv[2];
@@ -10,6 +11,10 @@ const command = process.argv[2];
 switch (command) {
   case "init":
     createGitDirectory();
+    break;
+  case "cat-file":
+    const hash = process.argv[4];
+    readGitBlob(hash);
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -22,4 +27,11 @@ function createGitDirectory() {
 
   fs.writeFileSync(path.join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
   console.log("Initialized git directory");
+}
+
+function readGitBlob(hash){
+    const compressed_data = fs.readFileSync(path.join(process.cwd(), ".git", "objects",hash.slice(0,2),hash.slice(2)));
+    const Data_Unzip = zlib.inflateSync(compressed_data);
+    const ans = Data_Unzip.to_string().split('\0')[1];
+    process.stdout.write(ans);
 }
